bb9152b9d6af45a659aca4b4629a908486d9a615
Harun Kaya <harungaming16@gmail.com>
2026-04-30 11:14:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2737592612-58d19",
+  "appId": "1:783430381248:web:0375e06137270676cdc725",
+  "apiKey": "AIzaSyBeoeOcScqrdTjUNsydlaapCPwZEw8oTPQ",
+  "authDomain": "studio-2737592612-58d19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "783430381248"
+};
